6336a604e6914fa3d4eb338cea4a5e503821f467
Kozatron <70766347+Kozatron@users.noreply.github.com>
2023-04-14 14:44:10 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -7,5 +7,13 @@ public class Node {
     Node(int x, int y) {
         this.x = x;
         this.y = y;
-    }  
+    } 
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
 }
